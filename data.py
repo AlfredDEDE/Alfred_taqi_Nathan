c6cb5a74200bec37c61155012bd1baf5aeaff450
Fammacito <113697946+Fammacito@users.noreply.github.com>
2022-10-21 15:14:50 +0200

    data liste
    la liste des prenoms et des noms

---
+++ data.py
@@ -0,0 +1,8 @@
+liste_prenom = ["sam","johny,"mat","remy","sacha","eva","usama","justin","gator"             
+               ,"jean","bubblebea","ines","paul","hugo","lebron","david","lana",
+               "aubin","sylvain","masdak"]
+
+liste_nom = ["faitmal","sinz","legamos","fasol","touille","antouilette","fémal",
+             "ptipeu","laligator","neimar","lemagatron","alamaternite",
+             "emploi","boss","james","michigan","rhoades","sahalor","duryf","code
+             penal"]
