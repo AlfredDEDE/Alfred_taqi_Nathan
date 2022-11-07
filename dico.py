dbd98ffede4306b7628587f364ff85363050e34d
AlfredDEDE <113697760+AlfredDEDE@users.noreply.github.com>
2022-11-07 15:49:36 +0100

    Update dico.py
    remise a niv

--- dico.py
+++ dico.py
@@ -1,101 +1,16 @@
 
+#from data import *
 
+#from outils import *
 
 
 dico = {\
 
-    "sam" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
+    "Prenom" : {"Francais" : creer_des_notes(), "Math" : creer_des_notes(), \
+                "Histoire-Geo" : creer_des_notes() , "Anglais" :creer_des_notes(), \
+                "Espagnol" : creer_des_notes() }
 
-    "johny" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "mat" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "remy" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "sacha" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "eva" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "usama" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "justin" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "gator" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "jean" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "bubblebea" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "ines" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "paul" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "hugo" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "Prenom" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "Prenom" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "Prenom" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "Prenom" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "Prenom" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "Prenom" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    "Prenom" : {"Francais" : creer_note() , "Math" : creer_note(), \
-                "Histoire-Geo" : creer_note() , "Anglais" : creer_note(), \
-                "Espagnol" : creer_note() } \
-
-    
-
-
-    
-    }
-
-
-
+                }
 
 
 
