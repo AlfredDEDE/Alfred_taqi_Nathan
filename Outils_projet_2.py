c1b1aac66662fbb7af73d1ea999d21272de4330d
AlfredDEDE <113697760+AlfredDEDE@users.noreply.github.com>
2022-11-13 16:04:11 +0100

    Update Outils_projet_2.py

--- Outils_projet_2.py
+++ Outils_projet_2.py
@@ -3,22 +3,18 @@ from data import *
 def est_bon_prenom (a):
     return a in liste_prenoms
 
-def pas_bon_prenom (a):
-    return not (liste_prenoms (a))
-
 def est_bon_nom (a):
     return a in liste_noms
 
-def pas_bon_nom (a):
-    return not (liste_noms (a))
+def verif_nom_ou_prenom_exist(a):
 
-def verif_nom_ou_prenom_exist (a) :
-    if pas_bon_prenom == False :
-        return (est_un_bon_prenom())
-    if pas_bon_nom == False :
-        return (est_un_bon_nom())
+    if est_bon_prenom (a) == True :
+        return (est_bon_prenom(a))
+    if est_bon_nom(a)== True :
+        return (est_bon_nom(a))
     else :
-        print(id_pas_valide)
+        return False
+
 def creer_une_note():
     notes.append(randint(0, 20))
     return notes
