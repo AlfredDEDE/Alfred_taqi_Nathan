1d78752e9eb2ffa4be6d943fa54b2b02bfb204e2
taqz25 <113697277+taqz25@users.noreply.github.com>
2022-11-13 14:56:58 +0100

    Dim 13 nov 15h

--- Outils_projet_2.py
+++ Outils_projet_2.py
@@ -1,5 +1,18 @@
-def is_a_valid_f_name (a):
+def est_bon_prenom (a):
     return a in liste_prenoms
+
+def pas_bon_prenom (a):
+    return not (liste_prenoms (a))
+
+def est_bon_nom (a):
+    return a in liste_noms
+
+def pas_bon_nom (a):
+    return not (liste_noms (a))
+
+def est_un_bon_prenom (a):
+    return a in liste_prenoms
+##
 
 def is_not_a_valid_f_name (a):
     return not (liste_prenoms (a))
@@ -9,30 +22,13 @@ def is_a_valid_name (a):
 
 def is_not_a_valid_f_name (a):
     return not (liste_noms (a))
+##
 
-def Noms (a) :
-    if a in liste_prenom :
-        print(is_a_valid_f_name(a))
+def Prenoms (a) :
+    if a in liste_prenoms :
+        print(est_bon_prenom(a))
     if a in liste_nom :
-        print(is_a_valid_name)
-
-def is_a_valid_f_name (a):
-    return a in liste_prenoms
-
-def is_not_a_valid_f_name (a):
-    return not (liste_prenoms (a))
-
-def is_a_valid_name (a):
-    return a in liste_noms
-
-def is_not_a_valid_f_name (a):
-    return not (liste_noms (a))
-
-def Noms (a) :
-    if a in liste_prenom :
-        print(is_a_valid_f_name (a))
-    if a in liste_nom :
-        print(is_not_a_valid_name (a))
+        print(est_bon_nom)
     else :
         print("Le nom se trouve pas dans la base de donnes")
 
