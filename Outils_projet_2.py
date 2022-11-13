87aa02df669d412ed188aadbc8024025d1fce55c
taqz25 <113697277+taqz25@users.noreply.github.com>
2022-11-13 16:47:36 +0100

    Outils

--- Outils_projet_2.py
+++ Outils_projet_2.py
@@ -36,9 +36,29 @@ def moyenne (notes):
         reponse = moyenne
     return reponse
 
-def moyenne_eleves_gen (nom):
+'''def moyenne_eleves_gen (nom):
     for i in range 21 :
         creer_des_notes()
-        moyenne(notes)
+        moyenne(notes)'''
+#
+
+def savoir_notes_eleves_sup_n (n):
+    moyenne_sup_n = []
+    for i in range 21 :
+        if moyenne_gen >= n:
+            moyenne_sup_n.append()
+    return moyenne_sup_n 
+
+def savoir_notes_eleves_inf_n (n):
+    moyenne_inf_n = []
+    for i in range 21 :
+        if moyenne_gen <= n:
+            moyenne_inf_n.append()
+    return moyenne_inf_n
+
+def savoir_nbr_eleves_sup_n (savoir_notes_eleves_sup_n):
+    len(savoir_notes_eleves_sup_n())
+def savoir_nbr_eleves_inf_n (savoir_notes_eleves_inf_n) :
+    len(savoir_notes_eleves_inf_n())
         
 
