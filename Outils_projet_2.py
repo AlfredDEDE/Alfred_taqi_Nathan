2f441ac5218481c0f08dcc1a3e36397e07763732
taqz25 <113697277+taqz25@users.noreply.github.com>
2022-11-13 15:33:53 +0100

    Outils

--- Outils_projet_2.py
+++ Outils_projet_2.py
@@ -1,21 +1,5 @@
 from data import *
 
-
-def est_bon_prenom (a):
-    return a in liste_prenoms
-
-def pas_bon_prenom (a):
-    return not (liste_prenoms (a))
-
-def est_bon_nom (a):
-    return a in liste_noms
-
-def pas_bon_nom (a):
-    return not (liste_noms (a))
-
-
-##
-
 '''def verif_nom_ou_prenom_exist (a) :
     if a in liste_prenoms :
         print(est_bon_prenom(a))
@@ -31,3 +15,41 @@ def verif_nom_ou_prenom_exist (a) :
         return (est_un_bon_nom())
     else :
         print(id_pas_valide)
+
+def est_bon_prenom (a):
+    return a in liste_prenoms
+
+def pas_bon_prenom (a):
+    return not (liste_prenoms (a))
+
+def est_bon_nom (a):
+    return a in liste_noms
+
+def pas_bon_nom (a):
+    return not (liste_noms (a))
+
+def moyenne (notes,coef):
+    somme = 0.0
+    for i in range (len(notes)):
+        somme = somme + notes [i] * coef [i]
+        moyenne = somme / sum(float(coef))
+        reponse = moyenne
+    return reponse
+
+notes = list(input('Notes?'))
+coef = list(input('coef?'))
+
+print (str(moyenne))
+
+def creer_une_note():
+    marks.append(randint(0, 20))
+    return marks
+
+def determiner_le_nombre_de_note():
+    length = randint(3, 8)
+    return length
+
+def creer_des_notes():
+    for i in range(determiner_le_nombre_de_note()):
+        creer_une_note()
+    return marks
