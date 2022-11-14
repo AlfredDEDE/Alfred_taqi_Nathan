862ad2416f2a5c7625bb30f52487110a5511f177
taqz25 <113697277+taqz25@users.noreply.github.com>
2022-11-14 15:37:02 +0100

    Update Outils_projet_2.py

--- Outils_projet_2.py
+++ Outils_projet_2.py
@@ -82,8 +82,8 @@ def savoir_nbr_eleves_sup_n (savoir_notes_eleves_sup_n):
 def savoir_nbr_eleves_inf_n (savoir_notes_eleves_inf_n) :
     len(savoir_notes_eleves_inf_n())
 
-#La liste et/ou le nombre d’élèves ayant une moyenne en Maths supérieure à n/20
+#La liste et/ou le nombre decimale�ves ayant une moyenne en Maths supérieure à n/20
 
-#La liste et/ou le nombre d’élèves ayant n moyennes supérieures à m/20
+#La liste et/ou le nombre decimale�ves ayant n moyennes supérieures à m/20
 
-#La liste et/ou le nombre d’élèves ayant une moyenne générale compris entre n1/20 et n2/20
+#La liste et/ou le nombre decimale�ves ayant une moyenne générale compris entre n1/20 et n2/20
